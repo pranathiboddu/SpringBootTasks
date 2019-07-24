45800327b02dd42a29d11a75afd8dc8545d0b9d8
pranathiboddu <pranathiboddu37@gmail.com>
2019-07-24 19:39:36 +0530

    using contextrefreshedevent and commandlinerunner done

--- src/main/java/com/stackroute/muzixapplication/MuzixapplicationApplication.java
+++ src/main/java/com/stackroute/muzixapplication/MuzixapplicationApplication.java
@@ -1,14 +1,51 @@
 package com.stackroute.muzixapplication;
 
+import com.stackroute.muzixapplication.domain.Music;
+import com.stackroute.muzixapplication.exceptions.TrackAlreadyExistsException;
+import com.stackroute.muzixapplication.service.MusicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MuzixapplicationApplication {
+//implemented commandlinerunner and extended SpringBootServletInitializer
+public class MuzixapplicationApplication extends SpringBootServletInitializer implements CommandLineRunner
+{
+ //implemented methods
+	MusicService musicService;
+	@Autowired
+	public void setMusicService(MusicService musicService) {
+		this.musicService = musicService;
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources(MuzixapplicationApplication.class);}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuzixapplicationApplication.class, args);
 	}
 
-}
+	//setting values to the music
+	@Override
+	public void run (String...args) throws Exception
+		{
+			try {
+				musicService.saveTrack(new Music(6, "chainsmokers", "Excellent"));
+				musicService.saveTrack(new Music(7, "one direction", "Excellent"));
+				musicService.saveTrack(new Music(8, "vidyavox", "Excellent"));
+			}
+			catch (TrackAlreadyExistsException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+
+
+
+
--- src/main/java/com/stackroute/muzixapplication/config/WebConfiguration.java
+++ src/main/java/com/stackroute/muzixapplication/config/WebConfiguration.java
@@ -1,14 +1,36 @@
 package com.stackroute.muzixapplication.config;
 
+import com.stackroute.muzixapplication.domain.Music;
+import com.stackroute.muzixapplication.exceptions.TrackAlreadyExistsException;
+import com.stackroute.muzixapplication.service.MusicService;
 import org.apache.catalina.servlets.WebdavServlet;
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @Configuration
 public class WebConfiguration {
 
+  MusicService musicService;
+    public WebConfiguration(MusicService musicService)
+    {
+        this.musicService=musicService;
+    }
+    //using ContextRefreshedEvent
+    @EventListener
+    public void seedData(ContextRefreshedEvent contextRefreshedEvent) {
+        try {
+            musicService.saveTrack(new Music(1, "chainsmokers", "Excellent"));
+            musicService.saveTrack(new Music(2, "one direction", "Excellent"));
+            musicService.saveTrack(new Music(3, "vidyavox", "Excellent"));
+        }
+        catch (TrackAlreadyExistsException ex) {
+            ex.printStackTrace();
+        }
+    }
     @Bean
     ServletRegistrationBean h2servletRegistration() //for h2 console view
     {
@@ -16,4 +38,6 @@ public class WebConfiguration {
         servletRegistrationBean.addUrlMappings("/console/*");
         return servletRegistrationBean;
     }
+
+
 }
